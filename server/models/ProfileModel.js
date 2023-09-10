defe483b918c7a801f5f8789215a3a1127d71d54
Suraj Pisal <surajpisal113@gmail.com>
2023-09-10 11:37:18 +0530

    fix user creating automatically issue

--- server/models/ProfileModel.js
+++ server/models/ProfileModel.js
@@ -4,12 +4,9 @@ const profileSchema = mongoose.Schema({
     name: String,
     email: {type: String, required: true, unique: true},
     phoneNumber: String,
-    businessName: String,
     contactAddress: String,
-    paymentDetails: String, 
     logo: String,
-    website: String,
-    userId: [String],
+    website: String
 })
 
 const Profile = mongoose.model('Profile', profileSchema)
