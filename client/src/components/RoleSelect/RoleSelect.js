f55bc48acf6c21483df9f48b3601b1105f0a62a1
Rasika Ghadge <90198493+rasikaghadge@users.noreply.github.com>
2023-10-14 19:24:14 +0530

    Added role selection page before login

---
+++ client/src/components/RoleSelect/RoleSelect.js
@@ -0,0 +1,73 @@
+import React from "react";
+import styles  from "./RoleSelect.module.css";
+import { useNavigate } from "react-router-dom";
+
+const RoleSelect = () => {
+  const navigate = useNavigate();
+  const user = JSON.parse(localStorage.getItem('profile'))
+
+    if(user) {
+        navigate('/homepage')
+    }
+  //    <Header/>
+  return (
+    
+    <div>
+      <div className={styles.header2}>
+        <img
+          style={{ width: "50px", cursor: "pointer" }}
+          onClick={() => navigate("/")}
+          src="https://i.postimg.cc/hGZKzdkS/logo.png"
+          alt="arc-invoice"
+        />
+      </div>
+      
+      <div className={styles.pageContainer}>
+        <section className={styles.hero}>
+          <h1>Login as a...</h1>
+          <RecruiterButton/>
+          <CandidateButton/>
+        
+        </section>
+      </div>
+    </div>
+  );
+};
+
+const CandidateButton = () => {
+    const navigate = useNavigate();
+  
+    const handleCandidateLogin = () => {
+      // Navigate to the login page with the role as a query parameter
+      navigate('/login?role=candidate');
+    };
+  
+    return (
+      <button
+          onClick={handleCandidateLogin}
+          className={styles.login}
+        >
+          Candidate
+        </button>
+    );
+  };
+
+  const RecruiterButton = () => {
+    const navigate = useNavigate();
+  
+    const handleRecruiterLogin = () => {
+      // Navigate to the login page with the role as a query parameter
+      navigate('/login?role=hr');
+    };
+  
+    return (
+        <button
+            onClick={handleRecruiterLogin}
+            className={styles.login}
+          >
+            Recruiter
+          </button>
+      );
+  };
+
+export default RoleSelect;
