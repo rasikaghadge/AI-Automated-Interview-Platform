b9043e2429f9f62993aadd33788632a002195768
Suraj Pisal <surajpisal113@gmail.com>
2023-10-22 09:47:11 +0530

    update room create route

--- server/controllers/interviews.js
+++ server/controllers/interviews.js
@@ -8,49 +8,21 @@ const API_BASE_URL = process.env.API_BASE_URL
 const VIDEOSDK_TOKEN = process.env.VIDEOSDK_TOKEN
 const API_AUTH_URL = null;
 
-export const getToken = async () => {
-	if (VIDEOSDK_TOKEN && API_AUTH_URL) {
-		throw new Error("Provide only ONE PARAMETER - either Token or Auth API");
-	} else if (VIDEOSDK_TOKEN) {
-		return VIDEOSDK_TOKEN;
-	} else if (API_AUTH_URL) {
-		try {
-			const response = await fetch(`${API_AUTH_URL}/get-token`);
-			const { token } = await response.json();
-			console.log(token);
-			return token;
-		} catch (error) {
-			console.error("error", error);
-		}
-	} else {
-		throw new Error("Please add a token or Auth Server URL");
-	}
-};
 
 export const createMeeting = async (req, res) => {
 	const url = `${API_BASE_URL}/v2/rooms`;
-	const headers = { Authorization: VIDEOSDK_TOKEN, "Content-Type": "application/json" };
+	const headers = { 
+		Authorization: req.headers.authorization.split(' ')[1],
+		"Content-Type": "application/json"
+	};
 	try {
-		const response = await fetch(url, { method: "POST", headers });
-		const { roomId } = await response.json();
+		const response = await axios.post(url, {}, { headers });
+		console.log(response)
+		const { roomId, links } = await response.data;
 		// return roomId;
-		res.status(200).json({ roomId });
+		res.status(200).json({ roomId, links });
 	} catch (error) {
 		console.error("error", error);
 	}
 };
 
-export const validateMeeting = async (req, res) => {
-	console.log(req.params);
-	const meetingId = req.params.meetingId;
-	const url = `${API_BASE_URL}/v2/rooms/validate/${meetingId}`;
-	const headers = { Authorization: VIDEOSDK_TOKEN, "Content-Type": "application/json" };
-	try {
-		const response = await fetch(url, { method: "GET", headers });
-		const result = await response.json();
-		// return result ? result.roomId === meetingId : false;
-		res.status(200).json({ result, valid: result.roomId === meetingId });
-	} catch (error) {
-		res.status(500).json({ error });
-	}
-};
--- server/routes/interviews.js
+++ server/routes/interviews.js
@@ -1,11 +1,8 @@
 import express from "express";
-import { getToken, createMeeting, validateMeeting } from "../controllers/interviews.js";
-import { hrAuth } from "../middleware/auth.js";
-
+import { createMeeting } from "../controllers/interviews.js";
+import { auth } from "../middleware/auth.js"
 const router = express.Router();
 
-router.get("/token", hrAuth, getToken);
-router.post("/create", createMeeting);
-router.post("/validate/:meetingId", validateMeeting);
+router.post("/create", auth, createMeeting);
 
 export default router;
