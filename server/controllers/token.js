ee0dedde146788f3610431b6c6b43fd787945c6f
Suraj Pisal <surajpisal113@gmail.com>
2023-10-22 09:23:05 +0530

    add permissions in token

--- server/controllers/token.js
+++ server/controllers/token.js
@@ -1,11 +1,26 @@
 import jwt from 'jsonwebtoken';
+import dotenv from 'dotenv'
 
 // Create a token for the user
 export const createToken = (email, id, role, expiresIn, SECRET) => {
+    var permissions = null;
+    if (role==='candidate') {
+        permissions = ['allow_join', 'ask_join']
+    } else if (role==='hr') {
+        permissions = ['allow_join', 'ask_join', 'allow_create', 'ask_create']
+    } else if (role==='admin') {
+        permissions = ['allow_join', 'ask_join', 'allow_create', 'ask_create', 'allow_delete', 'ask_delete']
+    }
+    else {
+        permissions = ['ask_join']
+    }
+    // referecence: https://docs.videosdk.live/react/guide/video-and-audio-calling-api-sdk/authentication-and-token#payload-while-generating-token
     return jwt.sign({ 
         email, 
         id,
-        role: role
+        role: role,
+        permissions: permissions,
+        apikey: process.env.VIDEOSDK_API_KEY
     }, SECRET, {
         expiresIn: expiresIn
     });
