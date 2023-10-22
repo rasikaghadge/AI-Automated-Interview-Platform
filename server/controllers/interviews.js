6d7d417777228e29713c882fdab73f3b80d51239
Suraj Pisal <surajpisal113@gmail.com>
2023-10-22 10:18:54 +0530

    add route to fetch rooms

--- server/controllers/interviews.js
+++ server/controllers/interviews.js
@@ -26,3 +26,47 @@ export const createMeeting = async (req, res) => {
 	}
 };
 
+// Reference: https://docs.videosdk.live/api-reference/realtime-communication/validate-room
+export const validateRoom = async (req, res) => {
+	const roomId = req.params.roomId;
+	const url = `${API_BASE_URL}/v2/rooms/validate/${roomId}`;
+	const headers = { 
+		Authorization: req.headers.authorization.split(' ')[1],
+	 	"Content-Type": "application/json" 
+	};
+  
+	try {
+	  const response = await axios.get(url, { headers });
+	  const data = response.data;
+	  const status = data ? data.roomId === roomId : false;
+	  if(status) {
+		  res.status(200).json({status, links: data.links, message: "Valid room code"});
+	  } else {
+		res.status(200).json({status, links: data.links, message: "Invalid room code"})
+	  }
+	} catch (error) {
+	  console.error("error", error);
+	  res.status(400).json({message: String(error)});
+	}
+  };
+
+  export const fetchRooms = async (req, res) => {
+	var url = `${API_BASE_URL}/v2/rooms?page=1&perPage=20`;
+	const headers = { 
+		Authorization: req.headers.authorization.split(' ')[1],
+		 "Content-Type": "application/json"
+		 };
+	try {
+		if(req.query.roomId!==undefined) {
+			console.log('roomid present');
+			 url= `https://api.videosdk.live/v2/rooms/${req.query.roomId}`;
+		}
+	  const response = await axios.get(url, { headers });
+	  const data = response.data;
+	  res.status(200).json(data);
+	} catch (error) {
+	  console.error("error", error);
+	  res.status(400).json({message: String(error)})
+	}
+  };
+  
--- server/routes/interviews.js
+++ server/routes/interviews.js
@@ -1,8 +1,11 @@
 import express from "express";
-import { createMeeting } from "../controllers/interviews.js";
+import { createMeeting, validateRoom, fetchRooms } from "../controllers/interviews.js";
 import { auth } from "../middleware/auth.js"
 const router = express.Router();
 
 router.post("/create", auth, createMeeting);
+router.get("/validate/:roomId", auth, validateRoom);
+router.get('/rooms', auth, fetchRooms);
+
 
 export default router;
