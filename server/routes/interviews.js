ccd8585a868b30daa61671242bbca664bfc70f13
Suraj Pisal <surajpisal113@gmail.com>
2023-11-09 14:17:36 +0530

    add meeting routes with dummy data

--- server/routes/interviews.js
+++ server/routes/interviews.js
@@ -8,4 +8,9 @@ router.get("/token", hrAuth, getToken);
 router.post("/create", createMeeting);
 router.post("/validate/:meetingId", validateMeeting);
 
+// meeting schedule
+router.post("/schedule", scheduleMeeting);
+router.get("/list", listMeetings);
+router.get("/:meetingId", getMeeting);
+
 export default router;
