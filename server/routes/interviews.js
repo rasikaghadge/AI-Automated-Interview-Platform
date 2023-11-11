214a60389adad873be01639a665789beddfb5fd0
Suraj Pisal <surajpisal113@gmail.com>
2023-11-11 09:34:47 +0530

    fix meeting routes

--- server/routes/interviews.js
+++ server/routes/interviews.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { getToken, createMeeting, validateMeeting } from "../controllers/interviews.js";
+import { getToken, createMeeting, validateMeeting, scheduleMeeting, listMeetings, getMeeting } from "../controllers/interviews.js";
 import { hrAuth } from "../middleware/auth.js";
 
 const router = express.Router();
