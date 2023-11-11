c08a811ec736b0ed527bc3655fc5949a61dda077
Suraj Pisal <surajpisal113@gmail.com>
2023-11-11 09:54:42 +0530

    remove unused routes

--- server/routes/interviews.js
+++ server/routes/interviews.js
@@ -1,12 +1,10 @@
 import express from "express";
-import { getToken, createMeeting, validateMeeting, scheduleMeeting, listMeetings, getMeeting } from "../controllers/interviews.js";
+import { createMeeting, scheduleMeeting, listMeetings, getMeeting } from "../controllers/interviews.js";
 import { hrAuth } from "../middleware/auth.js";
 
 const router = express.Router();
 
-router.get("/token", hrAuth, getToken);
-router.post("/create", createMeeting);
-router.post("/validate/:meetingId", validateMeeting);
+router.post("/create",hrAuth, createMeeting);
 
 // meeting schedule
 router.post("/schedule", scheduleMeeting);
