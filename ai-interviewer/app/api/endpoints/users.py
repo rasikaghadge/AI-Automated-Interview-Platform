3eb5bf154c4ff3dee72c8626890411efa9aaaf3b
Suraj Pisal <surajpisal113@gmail.com>
2024-02-03 15:28:10 +0530

    add user profile create using supabase

--- ai-interviewer/app/api/endpoints/users.py
+++ ai-interviewer/app/api/endpoints/users.py
@@ -1,5 +1,12 @@
+import base64
+
+import requests
 from app.core.session import supabase
-from app.schemas.requests import UserCreateRequest, UserLoginRequest
+from app.schemas.requests import (
+    UserCreateRequest,
+    UserLoginRequest,
+    UserProfileCreateRequest,
+)
 from app.schemas.responses import UserResponse
 from fastapi import APIRouter
 from fastapi.responses import JSONResponse
@@ -8,13 +15,33 @@ from gotrue.errors import AuthApiError
 router = APIRouter()
 
 
+def get_profile_picture_by_name(name: str) -> str | None:
+    response = requests.get(f"https://ui-avatars.com/api/?name={name}")
+    if response.status_code == 200:
+        base64_string = base64.b64encode(response.content).decode()
+        return base64_string
+    return None
+
+
+def create_profile(user: UserProfileCreateRequest):
+    try:
+        user.profile_picture = get_profile_picture_by_name(
+            f"{user.first_name} {user.last_name}")
+        data, count = supabase.table('profile').insert(
+            user.model_dump()).execute()
+        return data
+    except Exception as e:
+        return JSONResponse(content={"message": f"Unexpected Error. Try Again", "error": str(e)}, status_code=401)
+
+
 @router.post("/register", response_model=UserResponse)
 async def register_new_user(
     new_user: UserCreateRequest,
 ):
     try:
         res = supabase.auth.sign_up(new_user.model_dump()).model_dump()
-        print(type(res))
+        create_profile(UserProfileCreateRequest(
+            user_id=res['user']['id'], first_name=new_user.first_name, last_name=new_user.last_name))
         return UserResponse(access_token=res['session']['access_token'], refresh_token=res['session']['refresh_token'])
     except AuthApiError as e:
         return JSONResponse(content={"message": f"User already exist with {new_user.email}", "error": str(e)}, status_code=401)
@@ -27,7 +54,53 @@ async def login(
     new_user: UserLoginRequest,
 ):
     try:
-        res = supabase.auth.sign_in_with_password(new_user.model_dump()).model_dump()
+        res = supabase.auth.sign_in_with_password(
+            new_user.model_dump()).model_dump()
         return UserResponse(access_token=res['session']['access_token'], refresh_token=res['session']['refresh_token'])
     except AuthApiError as e:
         return JSONResponse(content={"message": f"User does not exist with {new_user.email}", "error": str(e)}, status_code=401)
+
+
+@router.post("/profile")
+async def create_user_profile(
+    user: UserProfileCreateRequest,
+):
+    return create_profile(user)
+
+
+@router.get("/profile")
+async def get_user_profile(
+    user_id: str,
+):
+    try:
+        data, count = supabase.table('profile').select(
+            "*").eq('user_id', user_id).execute()
+        return data
+    except Exception as e:
+        return JSONResponse(content={"message": f"Unexpected Error. Try Again", "error": str(e)}, status_code=401)
+    
+
+@router.get("/profile/{user_id}")
+async def get_user_profile(
+    user_id: str,
+):
+    try:
+        data, count = supabase.table('profile').select(
+            "*").eq('user_id', user_id).execute()
+        return data
+    except Exception as e:
+        return JSONResponse(content={"message": f"Unexpected Error. Try Again", "error": str(e)}, status_code=401)
+    
+
+@router.put("/profile")
+async def update_user_profile(
+    user: UserProfileCreateRequest,
+):
+    try:
+        data, count = supabase.table('profile').update(
+            user.model_dump()).eq('user_id', user.user_id).execute()
+        return data
+    except Exception as e:
+        return JSONResponse(content={"message": f"Unexpected Error. Try Again", "error": str(e)}, status_code=401)
+    
+
--- ai-interviewer/app/schemas/requests.py
+++ ai-interviewer/app/schemas/requests.py
@@ -1,30 +1,66 @@
+from typing import List, Literal
+
 from pydantic import BaseModel, EmailStr
 
 
-class BaseRequest(BaseModel):
+class BaseUserRequest(BaseModel):
     email: EmailStr
     password: str
 
 
-class RefreshTokenRequest(BaseRequest):
-    refresh_token: str
-
-
-class UserUpdatePasswordRequest(BaseRequest):
-    password: str
-
-
-class UserCreateRequest(BaseRequest):
+class UserCreateRequest(BaseUserRequest):
     first_name: str
     last_name: str
+    role: Literal['candidate', 'hr'] = 'candidate'
+    profile: int | str | EmailStr | None = None
 
 
-class UserLoginRequest(BaseRequest):
-    email: EmailStr
-    password: str
+class UserLoginRequest(BaseUserRequest):
+    pass
 
 
-class InterviewBaseRequest(BaseRequest):
+class SocialLinks(BaseModel):
+    name: str
+    link: str
+
+
+class EducationDuration(BaseModel):
+    start: str
+    end: str
+
+
+class Education(BaseModel):
+    degree: str
+    college: str
+    duration: EducationDuration
+
+
+class Experience(BaseModel):
+    company: str
+    role: str
+    duration: EducationDuration
+    description: str
+
+
+class ExternalAttributes(BaseModel):
+    socialLinks: List[SocialLinks] | None = None
+    education: List[Education] | None = None
+    experience: List[Experience] | None = None
+
+
+class UserProfileCreateRequest(BaseModel):
+    user_id: str
+    first_name: str | None = None
+    last_name: str | None = None
+    country: str | None = 'India'
+    profile_picture: str | None = None
+    interviews: List[str] | None = None
+    company: str | None = None  # for HR
+    experience: int = 0
+    external_attributes: ExternalAttributes | None = None
+
+
+class InterviewBaseRequest(BaseModel):
     title: str
     description: str
 
