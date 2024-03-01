d42e5d0c1a3f15a2b24bdd9d956376f58c76fef9
Suraj Pisal <surajpisal113@gmail.com>
2024-03-01 21:50:17 +0530

    fix settings

--- server/app/core/config.py
+++ server/app/core/config.py
@@ -1,45 +1,33 @@
 
 
-import tomllib
-from functools import cached_property
+import os
 from pathlib import Path
 from typing import Literal
 
-from pydantic import AnyHttpUrl, EmailStr, PostgresDsn, computed_field
+from pydantic import AnyHttpUrl
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 PROJECT_DIR = Path(__file__).parent.parent.parent
-with open(f"{PROJECT_DIR}/pyproject.toml", "rb") as f:
-    PYPROJECT_CONTENT = tomllib.load(f)["tool"]["poetry"]
 
 
 class Settings(BaseSettings):
     # CORE SETTINGS
-    SECRET_KEY: str
+    SECRET_KEY: str = os.getenv('SECRET_KEY')
     ENVIRONMENT: Literal["DEV", "PYTEST", "STG", "PRD"] = "DEV"
     SECURITY_BCRYPT_ROUNDS: int = 12
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 11520  # 8 days
     REFRESH_TOKEN_EXPIRE_MINUTES: int = 40320  # 28 days
     BACKEND_CORS_ORIGINS: list[AnyHttpUrl] = []
     ALLOWED_HOSTS: list[str] = ["*"]
-
-    # PROJECT NAME, VERSION AND DESCRIPTION
-    PROJECT_NAME: str = PYPROJECT_CONTENT["name"]
-    VERSION: str = PYPROJECT_CONTENT["version"]
-    DESCRIPTION: str = PYPROJECT_CONTENT["description"]
-
     # POSTGRESQL DEFAULT DATABASE
-    DEFAULT_DATABASE_HOSTNAME: str
-    DEFAULT_DATABASE_USER: str
-    DEFAULT_DATABASE_PASSWORD: str
-    DEFAULT_DATABASE_PORT: int
-    DEFAULT_DATABASE_DB: str
+    DEFAULT_DATABASE_HOSTNAME: str = os.getenv('DEFAULT_DATABASE_HOSTNAME')
+    DEFAULT_DATABASE_USER: str = os.getenv('DEFAULT_DATABASE_USER')
+    DEFAULT_DATABASE_PASSWORD: str = os.getenv('DEFAULT_DATABASE_PASSWORD')
+    DEFAULT_DATABASE_PORT: int = os.getenv('DEFAULT_DATABASE_PORT')
+    DEFAULT_DATABASE_DB: str = os.getenv('DEFAULT_DATABASE_DB')
 
-    # FIRST SUPERUSER
-    FIRST_SUPERUSER_EMAIL: EmailStr
-    FIRST_SUPERUSER_PASSWORD: str
-    # Ai
-    ELEVENLABS_API_KEY: str
+    # AI
+    ELEVENLABS_API_KEY: str = os.getenv('ELEVENLABS_API_KEY')
 
     model_config = SettingsConfigDict(
         env_file=f"{PROJECT_DIR}/.env", case_sensitive=True
@@ -47,4 +35,3 @@ class Settings(BaseSettings):
 
 
 settings: Settings = Settings()  # type: ignore
-
