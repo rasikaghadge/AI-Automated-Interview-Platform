a62e4f464a3cc24e4566bf26e16e929a2a8ffda0
Suraj Pisal <surajpisal113@gmail.com>
2024-04-18 06:56:24 +0530

    use simple formtting

--- client/src/components/SeeScheduledInterviews/EvaluationPopup.js
+++ client/src/components/SeeScheduledInterviews/EvaluationPopup.js
@@ -1,19 +1,21 @@
 import React from "react";
 
 const EvaluationPopup = ({ evaluationData, onClose }) => {
-    console.log(evaluationData)
-  const sections = evaluationData ? evaluationData.split("\n") : [];
-
+    let result = '';
+    try {
+         result = evaluationData.replace(/\\n/g, '\n');
+    } catch (error) {
+         result = evaluationData;
+    }
   return (
     <div style={{ position: 'fixed', top: 0, left: 0, width: '100%', height: '100%', backgroundColor: 'rgba(0, 0, 0, 0.5)', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
       <div style={{ backgroundColor: 'white', padding: '20px', borderRadius: '10px', width: '80%', maxWidth: '500px' }}>
         <h2 style={{ margin: 0 }}>Evaluation</h2>
-        {sections.length > 0 ? (
-          sections.map((section, index) => (
-            <p key={index} style={{ marginTop: '20px', marginBottom: '20px' }}>
-              {section}
-            </p>
-          ))
+        {evaluationData ? (
+          <div style={{ marginTop: '20px', marginBottom: '20px' }}>
+            {/* Display evaluation data here */}
+            <p>{result}</p>
+          </div>
         ) : (
           <p>No evaluation data available.</p>
         )}
